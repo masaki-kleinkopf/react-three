9974ee6b13b7ada19172215b64522563c0bca368
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2024-02-06 12:06:27 -0800

    edit positions

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <spotLight position={[20, 20, 20]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-20, -20, -20]} decay={0} intensity={Math.PI} />
       <Text position={[0, 0, 0]} />
       {/* <OrbitControls /> */}
     </Canvas>
